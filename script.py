c7afc7e5828e0d477c8eeb59ae2ce23bd24fa17b
Lsimelus <lsimelus@gmail.com>
2025-07-19 23:03:50 -0400

    Add rental income

--- script.py
+++ script.py
@@ -1,5 +1,57 @@
 import csv
 
+abbreviation_to_state = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
 
 def format_string(text):
   """Converts a string to lowercase and replaces spaces with hyphens."""
@@ -12,7 +64,6 @@ rate_mapper = {}
 PROPERTY_TAX_AND_COUNTY = "property_tax_county.csv"
 with open(PROPERTY_TAX_AND_COUNTY, 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=",")
-    rate_mapper = {}
     for line in csv_reader:
         if line["State"] in rate_mapper:
             state = rate_mapper[line["State"]]
@@ -26,7 +77,20 @@ with open(PROPERTY_TAX_AND_COUNTY, 'r') as csv_file:
                 "median_value": line["Median Housing Value, 2023 ($)"],
                 "median_tax": line["Effective Property Tax Rate (2023)"].strip("%")
             }
-        
+            
+RENT_AND_SIZE = "rent_and_size.csv"
+rent_and_size_mapper = {}   
+with open(RENT_AND_SIZE, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+    for line in csv_reader:
+        if not(line["State"] == "DC"):
+            state = abbreviation_to_state[line["State"]]
+            if not(state in rent_and_size_mapper):
+                rent_and_size_mapper[state] = {line["RegionName"] : line["2025-03-31"]}
+            else:
+                cities = rent_and_size_mapper[state]
+                cities[line["RegionName"]] = line["2025-03-31"]
+      
 
 CITY_STATE_COUNTY ="city_state_county.csv"
 data = {} 
@@ -61,17 +125,19 @@ for key in providences_to_remove:
     data.pop(key, None)
     
 with open(OUTPUT_FILE, 'w', newline='') as csvfile:
-    fieldnames = ['state', 'city', 'county', 'median_value', 'median_tax']
+    fieldnames = ['state', 'city', 'county', 'median_value', 'median_tax', 'median_rent']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=",")
 
     writer.writeheader()
     
     for state, cities_dict in data.items():
+        
         for city, city_info in cities_dict.items():
             writer.writerow({
                 fieldnames[0]: format_string(state),
                 fieldnames[1]: format_string(city),
                 fieldnames[2]: format_string(city_info["county"]),
                 fieldnames[3]: city_info.get("median_value", ""),
-                fieldnames[4]: city_info.get("median_tax", "")
+                fieldnames[4]: city_info.get("median_tax", ""),
+                fieldnames[5]: rent_and_size_mapper[state].get(city, "")
             })
